3e473fed1909693a86413f94c7d42726a8c7d35b
Caio Willian <caiow.willian@gmail.com>
2021-07-29 23:29:07 -0300

    chore: workerpool

---
+++ 14. concurrency/worker-pool/worker-pool.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	tasks := make(chan int, 45)
+	results := make(chan int, 45)
+
+	go worker(tasks, results)
+	go worker(tasks, results)
+	go worker(tasks, results)
+	go worker(tasks, results)
+
+	for i := 0; i < 45; i++ {
+		tasks <- i
+	}
+	close(tasks)
+
+	for i := 0; i < 45; i++ {
+		result := <-results
+		fmt.Println(result)
+	}
+}
+
+func worker(tasks <-chan int, results chan<- int) {
+	for task := range tasks {
+		results <- fibonacci(task)
+	}
+}
+
+func fibonacci(position int) int {
+	if position <= 1 {
+		return position
+	}
+	return fibonacci(position-2) + fibonacci(position-1)
+}
