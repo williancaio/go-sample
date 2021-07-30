08c2c0b1ffba85b7c66e9dd7f69d71b8b4495ba5
Caio Willian <caiow.willian@gmail.com>
2021-07-30 18:18:05 -0300

    chore: json

---
+++ 15. json/json.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type user struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func main() {
+	u := user{"123", 1}
+	fmt.Println(u)
+
+	// convert to json
+	ub, err := json.Marshal(u)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	uj := bytes.NewBuffer(ub)
+	fmt.Println(uj)
+
+	// json to struct
+	var user user
+	js := `{
+		"name": "test",
+		"age": 1
+		}`
+
+	json.Unmarshal([]byte(js), &user)
+
+	fmt.Println(user)
+}
